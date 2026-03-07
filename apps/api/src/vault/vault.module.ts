76bad4271534d9aeb77fabcd9ea9f083c5a21691
wassim205 <wassimelmorabit7@gmail.com>
2026-03-07 22:32:09 +0000

    GS-44: (fix) add PrismaModule and CommonModule imports to VaultModule

--- apps/api/src/vault/vault.module.ts
+++ apps/api/src/vault/vault.module.ts
@@ -3,9 +3,11 @@ import { VaultService } from './vault.service';
 import { VaultController } from './vault.controller';
 import { UsersModule } from 'src/users/users.module';
 import { AuthModule } from 'src/auth/auth.module';
+import { CommonModule } from 'src/common/common.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, CommonModule, PrismaModule],
   controllers: [VaultController],
   providers: [VaultService],
   exports: [VaultService],
