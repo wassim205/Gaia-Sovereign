4ce380bf3573f172415035ac5f2c7db9e9a5c2af
wassim205 <wassimelmorabit7@gmail.com>
2026-03-13 13:35:29 +0000

    fix(api): correct jest path mapping and test DI

--- apps/api/src/auth/auth.controller.spec.ts
+++ apps/api/src/auth/auth.controller.spec.ts
@@ -1,5 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { JwtService } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PasswordService } from './services/password.service';
+import { CryptoService } from './services/crypto.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +13,14 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        AuthService,
+        JwtService,
+        PasswordService,
+        CryptoService,
+        UsersService,
+        PrismaService,
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
