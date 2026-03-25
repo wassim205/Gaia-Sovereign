9a60ccefd2efa2f7d0343aab53dea28f6a0498d2
wassim205 <wassimelmorabit7@gmail.com>
2026-03-25 14:57:55 +0100

    GS-170: Admin Guard/Authorization

---
+++ apps/api/src/auth/guards/admin.guard.ts
@@ -0,0 +1,24 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import type { Request } from 'express';
+import type { CurrentUserData } from 'src/auth/decorators/current-user.decorator';
+
+@Injectable()
+export class AdminGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context
+      .switchToHttp()
+      .getRequest<Request & { user: CurrentUserData }>();
+    const user = request.user;
+
+    if (!user || user.role !== 'ADMIN') {
+      throw new ForbiddenException('Admin access required');
+    }
+
+    return true;
+  }
+}
